fe07eeae8abca74da6ff7d833876b1c4a140a96b
treska23 <treska23@hotmail.com>
2026-07-27 18:47:32 +0200

    Añade referencia al servicio de navegación de YouTube

--- MainWindow.PlaylistImportFixes.cs
+++ MainWindow.PlaylistImportFixes.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using DrumPracticeStudio.Models;
+using DrumPracticeStudio.Services;
 
 namespace DrumPracticeStudio;
 
